0076bc193d215df88f30ecb90e254b777c8f1bee
ragul1810 <lugarlugar9991@gmail.com>
2025-12-17 13:38:46 +0530

    Create page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <div className="container">
+      <h1>Contact us</h1>
+      <p>📞 +91-9876543210</p>
+      <p>📧 info@myresort.com</p>
+      <p>📍 123 Green Valley Road, Coimbatore, TN, India</p>
+    </div>
+  )
+}
